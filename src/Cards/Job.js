77102d63ad2e5c72371c05800572a6822987acda
Ben Garlock <ben.garlock@gmail.com>
2021-02-02 14:51:59 -0500

    added job title buttons

--- src/Cards/Job.js
+++ src/Cards/Job.js
@@ -3,8 +3,13 @@ import "../Stylesheets/Job.css"
 
 class Job extends React.Component {
 
-    onClickHandler = () => {
+    onClickHandler = (e) => {
         navigator.clipboard.writeText(this.props.job)
+        const buttons = document.getElementsByClassName("job-selected")
+
+        const button = e.target
+        button.className = "job-selected"
+
     }
 
     render() {
