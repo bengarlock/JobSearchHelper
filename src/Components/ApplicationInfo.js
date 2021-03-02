d8f7e2c1a561a6a1df3a673bcfa0eec7c9eb63aa
github username <github email address>
2021-03-02 11:18:18 -0500

    server deployment

--- src/Components/ApplicationInfo.js
+++ src/Components/ApplicationInfo.js
@@ -7,6 +7,7 @@ class ApplicationInfo extends React.Component {
         date_applied: '',
         company_name: '',
         job_title: '',
+        delete_clicked: false,
     }
 
     onClickHandler = (e) => {
@@ -19,12 +20,18 @@ class ApplicationInfo extends React.Component {
             }
         }
         if (e.target.className === "delete") {
-            fetch(url, packet)
-                .then(this.props.renderApplications())
-                .then(this.props.toggleApplication(null))
-                .then(this.props.updateApplications(this.props.application, "remove"))
-                .then(this.props.removeAppFromSearch(this.props.application))
+            if (this.state.delete_clicked) {
+                fetch(url, packet)
+                    .then(this.props.renderApplications())
+                    .then(this.props.toggleApplication(null))
+                    .then(this.props.updateApplications(this.props.application, "remove"))
+                    .then(this.props.removeAppFromSearch(this.props.application))
+            } else {this.setState({delete_clicked: true}) }
+
+        } else {
+            this.setState({ delete_clicked: false})
         }
+
     }
 
 
@@ -49,7 +56,7 @@ class ApplicationInfo extends React.Component {
                         {this.props.application.status}
                     </div>
                     <div className="delete" onClick={this.onClickHandler}>
-                        DELETE
+                        {this.state.delete_clicked ? "CONFIRM DELETE?" : "DELETE"}
                     </div>
                 </div>
 
